5dd67559ac177f0d3ce23651da293b67e286f96f
tanwar57 <55880545+tanwar57@users.noreply.github.com>
2020-10-08 13:02:33 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 echo "hello world"
+git config --global user.email "rtanwar57@gmail.com"
+git config --global user.name "tanwar57"
 git clone https://github.com/tanwar57/testRelease.git
 cd testRelease
 git status
 git branch
 git checkout master
 git checkout -b release
-git tag -a v1.5 -m "release candidate 1"
+git tag -a v1.6 -m "release candidate 1"
 echo "hello branch" > branch.txt
 git add .
 git commit -m "added file"
-git push origin v1.5
+git push origin v1.6
+
 
