fdf5bf12411b39fa9dc152ac8c7478db69e471f9
tanwar57 <55880545+tanwar57@users.noreply.github.com>
2020-10-08 15:37:56 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,10 +9,6 @@ git status
 git branch
 git checkout master
 git checkout -b release
-git tag -a v1.6 -m "release candidate 1"
-echo "hello branch" > branch.txt
-git add .
-git commit -m "added file"
-git push origin v1.6
+git tag -a var.tag -m "release candidate 1"
 
 
